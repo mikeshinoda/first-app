f9f55fdbb8b8e7a97dc084e5e77151f01eb7d0e2
Akshit <akin.park@gmail.com>
2019-05-29 11:28:44 +0530

    getters n setters

---
+++ src/main/java/com/noon/app/models/SecondRoute.java
@@ -0,0 +1,11 @@
+package com.noon.app.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SecondRoute {
+    private int id;
+    private String message;
+}
--- src/main/java/com/noon/app/resource/MyHttp.java
+++ src/main/java/com/noon/app/resource/MyHttp.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.noon.app.models.SecondRoute;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -24,5 +26,15 @@ public class MyHttp {
         JsonObject object = new JsonObject();
         object.addProperty("title", "value");
         return Response.status(Response.Status.OK).entity(object.toString()).build();
+    }
+
+    @GET
+    @Path("/v1/secondRoute1")
+    public Response ingest1() {
+        log.info("HIT IN THE API-2");
+        SecondRoute res = new SecondRoute();
+        res.setId(1);
+        res.setMessage("hello");
+        return Response.status(Response.Status.OK).entity(res).build();
     }
 }
